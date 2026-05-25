6e07c353c14512d4af8c1b9c7f69e2eac5a8d89a
ByMoglii <e.tropeano@bluewin.ch>
2026-05-25 22:28:44 +0200

    fix

--- src/main/java/ch/by_moglii/zetaapi/api/ZetaAPI.java
+++ src/main/java/ch/by_moglii/zetaapi/api/ZetaAPI.java
@@ -12,32 +12,42 @@ public class ZetaAPI {
         provider = p;
     }
 
+    public static boolean isReady() {
+        return provider != null;
+    }
+
+    private static ZetaProvider getProvider() {
+        if (provider == null) {
+            throw new IllegalStateException("ZetaAPI is not initialized (provider = null)");
+        }
+        return provider;
+    }
+
     public static int getMaxViolations(String check, String type) {
-        return provider.getMaxViolations(check, type);
+        return getProvider().getMaxViolations(check, type);
     }
 
     public static int getViolations(UUID uuid, String check, String type) {
-        return provider.getViolations(uuid, check, type);
+        return getProvider().getViolations(uuid, check, type);
     }
 
     public static int getTotalViolations(UUID uuid) {
-        return provider.getTotalViolations(uuid);
+        return getProvider().getTotalViolations(uuid);
     }
 
     public static boolean isCheckEnabled(String check, String type) {
-        return provider.isCheckEnabled(check, type);
+        return getProvider().isCheckEnabled(check, type);
     }
 
     public static boolean isSetBackEnabled(String check, String type) {
-        return provider.isSetBackEnabled(check, type);
+        return getProvider().isSetBackEnabled(check, type);
     }
 
     public static String getPunishment(String check, String type) {
-        return provider.getPunishment(check, type);
+        return getProvider().getPunishment(check, type);
     }
 
     public static String getLanguage() {
-        return provider.getLanguage();
+        return getProvider().getLanguage();
     }
-
 }
